a0df37a696fa6ec05f961f6cd0513eda6af2be27
Ahnaf Muqset Haque <ahnafmuqset@gmail.com>
2024-03-05 14:45:40 -0800

    OrderItem layout and prelim design complete

--- front-end/src/components/pages/AboutUs/AboutUs.tsx
+++ front-end/src/components/pages/AboutUs/AboutUs.tsx
@@ -2,11 +2,27 @@ import Typography from "@mui/material/Typography";
 import { FC } from "react";
 import Stack from "@mui/material/Stack";
 
-
 export const AboutUs: FC = () => {
   return (
     <Stack>
-      <Typography
+      {/* <Header/> */}
+      <Stack>
+        <Typography>Who we are</Typography>
+        <Typography>
+          Culinary Canvas is an online platform that provides a solution to the
+          difficulties encountered by local chefs in offering authentic culinary
+          experiences. It serves as a platform for students, immigrants and
+          working professionals to access an array of authentic, chef-prepared
+          meals and catering services. The platform sets itself apart by
+          facilitating bespoke culinary journeys, delivering both convenience
+          and a diverse selection of curated meals directly to the consumer’s
+          door. As the sole provider of on-demand, economical, home-style cooked
+          dishes from neighborhood chefs, Culinary Canvas has carved out a
+          unique niche in the market. 
+        </Typography>
+      </Stack>
+      {/* <Footer/> */}
+      {/* <Typography
         sx={{
           fontFamily: "Montserrat",
           fontWeight: "700",
@@ -17,7 +33,7 @@ export const AboutUs: FC = () => {
         }}
       >
         About Us
-      </Typography>
+      </Typography> */}
     </Stack>
   );
 };
--- front-end/src/components/pages/Home/Home.tsx
+++ front-end/src/components/pages/Home/Home.tsx
@@ -1,23 +1,19 @@
 import Typography from "@mui/material/Typography";
 import { FC } from "react";
 import Stack from "@mui/material/Stack";
-
+import { OrderItem } from "../../OrderItem";
 
 export const Home: FC = () => {
   return (
-    <Stack>
-      <Typography
-        sx={{
-          fontFamily: "Montserrat",
-          fontWeight: "700",
-          fontSize: "42px",
-          fontStyle: "normal",
-          lineHeight: "59px",
-          letterSpacing: "1.68px",
-        }}
-      >
-        HOMEHOMEHOMEHOMEHOME
-      </Typography>
+    <Stack
+      flexDirection="row"
+      sx={{
+        width: "100%",
+        height: "400%",
+      }}
+    >
+      <OrderItem />
+      <OrderItem />
     </Stack>
   );
 };
