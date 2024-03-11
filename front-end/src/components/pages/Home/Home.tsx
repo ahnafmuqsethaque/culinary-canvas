f1e2bd79b5f3f54eb7079f5cf1f93361c988daee
Ahnaf Muqset Haque <ahnafmuqset@gmail.com>
2024-03-11 01:59:20 -0700

    cart page completed

--- front-end/src/components/pages/Home/Home.tsx
+++ front-end/src/components/pages/Home/Home.tsx
@@ -2,18 +2,50 @@ import Typography from "@mui/material/Typography";
 import { FC } from "react";
 import Stack from "@mui/material/Stack";
 import { OrderItem } from "../../OrderItem";
+import TextField from "@mui/material/TextField";
+import { OrderCounter } from "../../OrderCounter";
+import { CartItem } from "../../CartItem";
 
 export const Home: FC = () => {
   return (
     <Stack
-      flexDirection="row"
       sx={{
         width: "100%",
-        height: "400%",
+        height: "100%",
       }}
     >
-      <OrderItem />
-      <OrderItem />
+      <TextField
+        label="Food, Chefs, Drinks, etc."
+        variant="outlined"
+        InputProps={{ sx: { borderRadius: 50 } }}
+        sx={{ maxWidth: "1000px" }}
+      />
+      <OrderCounter />
+      <Typography
+        style={{
+          color: "black",
+          fontSize: 36,
+          fontFamily: "Montserrat",
+          fontWeight: "700",
+        }}
+      >
+        Order
+      </Typography>
+      <Stack
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="flex-start"
+        padding={4}
+        gap="16px"
+        sx={{
+          width: "100%",
+          height: "100%",
+          background: "#F3C2EE",
+        }}
+      >
+        <OrderItem />
+        <OrderItem />
+      </Stack>
     </Stack>
   );
 };
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AboutUs } from "./components/pages/AboutUs";
-import { Home } from './components/pages/Home';
+import { Home } from "./components/pages/Home";
+import { Cart } from "./components/pages/Cart";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
@@ -25,4 +27,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
